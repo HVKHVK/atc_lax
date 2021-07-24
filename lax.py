066e9cb1f4e4e46d5de8ce12d5f1093fc4218b9d
HVKHVK <vkasikaralar@gmail.com>
2021-07-24 22:00:34 +0300

    Improved Switching

--- lax.py
+++ lax.py
@@ -4,6 +4,7 @@ import sys
 import time
 import config as cfg
 
+#instance = vlc.Instance("--audio-visual=visual")
 s1 = vlc.MediaPlayer(cfg.SOUTH_TOWER)
 s2 = vlc.MediaPlayer(cfg.NORTH_TOWER)
 s3 = vlc.MediaPlayer(cfg.SOUTH_GROUND)
@@ -30,85 +31,68 @@ screen.blit(myfont.render('Off', False, (250, 250, 250)), (60, 280))
 screen.blit(myfont.render('Off', False, (250, 250, 250)), (260, 280))
 pygame.display.flip()
 
-status = [False, False, False, False, False, False]
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_t:
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(60, 60, 80, 80))
-                if not status[0]:
+                if not s1.is_playing():
                     s1.play()
                     time.sleep(1)
-                    status[0] = True
                     screen.blit(myfont.render('On', False, (250, 0, 250)), (60, 80))
                 else:
                     s1.stop()
-                    status[0] = False
                     screen.blit(myfont.render('Off', False, (250, 250, 250)), (60, 80))
                 pygame.display.flip()
             if event.key == pygame.K_y:
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(260, 60, 260, 80))
-                if not status[1]:
+                if not s2.is_playing():
                     s2.play()
                     time.sleep(1)
-                    status[1] = True
                     screen.blit(myfont.render('On', False, (250, 0, 250)), (260, 80))
                 else:
                     s2.stop()
-                    status[1] = False
                     screen.blit(myfont.render('Off', False, (250, 250, 250)), (260, 80))
                 pygame.display.flip()
             if event.key == pygame.K_g:
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(60, 170, 50, 20))
-                if not status[2]:
+                if not s3.is_playing():
                     s3.play()
                     time.sleep(1)
-                    status[2] = True
                     screen.blit(myfont.render('On', False, (250, 0, 250)), (60, 180))
                 else:
                     s3.stop()
-                    status[2] = False
                     screen.blit(myfont.render('Off', False, (250, 250, 250)), (60, 180))
                 pygame.display.flip()
             if event.key == pygame.K_h:
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(260, 170, 50, 20))
-                if not status[3]:
+                if not s4.is_playing():
                     s4.play()
                     time.sleep(1)
-                    status[3] = True
                     screen.blit(myfont.render('On', False, (250, 0, 250)), (260, 180))
                 else:
                     s4.stop()
-                    status[3] = False
                     screen.blit(myfont.render('Off', False, (250, 250, 250)), (260, 180))
                 pygame.display.flip()
             if event.key == pygame.K_b:
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(60, 270, 50, 20))
-                if not status[4]:
+                if not s5.is_playing():
                     s5.play()
                     time.sleep(1)
-                    status[4] = True
                     screen.blit(myfont.render('On', False, (250, 0, 250)), (60, 280))
                 else:
                     s5.stop()
-                    status[4] = False
                     screen.blit(myfont.render('Off', False, (250, 250, 250)), (60, 280))
                 pygame.display.flip()
             if event.key == pygame.K_n:
                 pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(260, 270, 50, 20))
-                if not status[5]:
+                if not s5.is_playing():
                     s6.play()
                     time.sleep(1)
-                    status[5] = True
                     screen.blit(myfont.render('On', False, (250, 0, 250)), (260, 280))
                 else:
                     s6.stop()
-                    status[5] = False
                     screen.blit(myfont.render('Off', False, (250, 250, 250)), (260, 280))
                 pygame.display.flip()
             if event.key == pygame.K_q:
                 sys.exit(1)
-
-
-
